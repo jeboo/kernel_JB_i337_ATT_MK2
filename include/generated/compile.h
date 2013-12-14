1c13c4d1dea3a2f42d4a5a4556a9308dba5e1111
Your Name <jeb00o@yahoo.com>
2013-12-14 14:21:47 -0800

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Nov 21 11:26:41 KST 2013"
+#define LINUX_COMPILE_BY "omsgsm"
+#define LINUX_COMPILE_HOST "sy918-desktop"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
